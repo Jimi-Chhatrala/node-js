2b0e895e881a1fb31e79f2a9a06d57d2c942912f
jimichhatrala <jimichhatrala8488@gmail.com>
2022-06-27 23:17:10 +0530

    Weather App Request

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,18 +1,54 @@
 const request = require("request");
 
-const url =
-  "http://api.weatherstack.com/current?access_key=952f83e27f3c2606f6cac19b0dff89c0&query=37.8267,-122.4233?units=f";
+// const url =
+//   "http://api.weatherstack.com/current?access_key=952f83e27f3c2606f6cac19b0dff89c0&query=37.8267,-122.4233&units=f";
 
-request({ url: url, json: true }, (error, response) => {
-  // console.log(response.body.current);
-  console.log(
-    response.body.current.weather_descriptions[0] +
-      ". It is currently " +
-      response.body.current.temperature +
-      " degrees out. It feels like " +
-      response.body.current.feelslike +
-      " degrees out."
-  );
+// const url =
+//   "http://api.weatherstack.com/current?access_key=952f83e27f3c2606f6cac19b0dff89c0&query=37.8267,-122.4233&units=f";
+
+// request({ url: url, json: true }, (error, response) => {
+//   // console.log(response.body.current);
+
+//   if (error) {
+//     console.log("Unable to connect to weather service!");
+//   } else if (response.body.error) {
+//     console.log("Unable to find location");
+//   } else {
+//     console.log(
+//       response.body.current.weather_descriptions[0] +
+//         ". It is currently " +
+//         response.body.current.temperature +
+//         " degrees out. It feels like " +
+//         response.body.current.feelslike +
+//         " degrees out."
+//     );
+//   }
+// });
+
+// const geocodeURL =
+//   "http://api.positionstack.com/v1/forward?access_key=aad27c86d1e2762115654e1674aea905&query=1600 Pennsylvania Ave NW, Washington DC&limit=1";
+
+// const geocodeURL =
+//   "http://api.positionstack.com/v1/forward?access_key=aad27c86d1e2762115654e1674aea905&query=&limit=1";
+
+const geocodeURL =
+  "http://api.positionstack.com/v1/forward?access_key=aad27c86d1e2762115654e1674aea905&query=Los%20Angles&limit=1";
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  // console.log(response.body.data[0].name);
+
+  if (error) {
+    console.log("Unable to connect to location services!");
+  }
+  // else if (response.body.error != "") {
+  //   // console.log(response.body.error != "");
+  //   console.log("Unable to find location. Try another search.");
+  // }
+  else {
+    const latitude = response.body.data[0].latitude;
+    const longitude = response.body.data[0].longitude;
+    console.log(latitude, longitude);
+  }
 });
 
 // request({ url: url }, (error, response) => {
