ea2294af1fa93ec0d17baa1e882ca0c35510392a
jimichhatrala <jimichhatrala8488@gmail.com>
2022-06-28 20:14:55 +0530

    Express serving object , array, html

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -3,19 +3,43 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello Express");
+  res.send("<h1>Hello Express</h1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help Page");
+  res.send("<h1>Help Page</h1>");
+});
+
+app.get("/object", (req, res) => {
+  res.send({
+    name: "Jimi",
+    age: 20,
+  });
+});
+
+app.get("/array", (req, res) => {
+  res.send([
+    {
+      name: "Jimi",
+      age: 20,
+    },
+    {
+      name: "Jen",
+      age: 16,
+    },
+  ]);
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Page");
+  res.send("<h1>About Page</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather Page");
+  //   res.send("Weather Page");
+  res.send({
+    forecast: "It is snowing",
+    location: "Boston",
+  });
 });
 
 app.listen(8000, () => {
