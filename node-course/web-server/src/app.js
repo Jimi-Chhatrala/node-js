d7aff65e41daf7bcbaa5d4122a51baf8f9c3e13d
jimichhatrala <jimichhatrala8488@gmail.com>
2022-07-01 12:01:38 +0530

    Advance Tempating Usin Partials Header Footer

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-
+const hbs = require("hbs");
 // console.log(__dirname);
 // console.log(path.join(__dirname, "../public"));
 // console.log(__filename);
@@ -8,11 +8,13 @@ const app = express();
 
 // Define path for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // setup static directory to serve
 app.get("", (req, res) => {
@@ -46,37 +48,37 @@ app.use(express.static(publicDirectoryPath));
 //   res.send("<h1>Help Page</h1>");
 // });
 
-app.get("/object", (req, res) => {
-  res.send({
-    name: "Jimi",
-    age: 20,
-  });
-});
+// app.get("/object", (req, res) => {
+//   res.send({
+//     name: "Jimi",
+//     age: 20,
+//   });
+// });
 
-app.get("/array", (req, res) => {
-  res.send([
-    {
-      name: "Jimi",
-      age: 20,
-    },
-    {
-      name: "Jen",
-      age: 16,
-    },
-  ]);
-});
+// app.get("/array", (req, res) => {
+//   res.send([
+//     {
+//       name: "Jimi",
+//       age: 20,
+//     },
+//     {
+//       name: "Jen",
+//       age: 16,
+//     },
+//   ]);
+// });
 
 // app.get("/about", (req, res) => {
 //   res.send("<h1>About Page</h1>");
 // });
 
-app.get("/weather", (req, res) => {
-  //   res.send("Weather Page");
-  res.send({
-    forecast: "It is snowing",
-    location: "Boston",
-  });
-});
+// app.get("/weather", (req, res) => {
+//   //   res.send("Weather Page");
+//   res.send({
+//     forecast: "It is snowing",
+//     location: "Boston",
+//   });
+// });
 
 app.listen(8000, () => {
   console.log("Server is up on port : 8000");
