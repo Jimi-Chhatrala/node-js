606d8a1dfd2ea0d21d30e247152d844dc216ea75
jimichhatrala <jimichhatrala8488@gmail.com>
2022-07-04 23:41:22 +0530

    Complete Weather App

--- node-course/web-server/public/js/app.js
+++ node-course/web-server/public/js/app.js
@@ -19,19 +19,30 @@ console.log("Client Side JavaScript file is loaded");
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+// messageOne.textContent = "From JS";
+
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const location = search.value;
 
+  messageOne.textContent = "loading...";
+  messageTwo.textContent = "";
+
   fetch(`http://localhost:8000/weather?address=${location}`).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
-          console.log(data.error);
+          // console.log(data.error);
+          messageOne.textContent = data.error;
         } else {
-          console.log(data.location);
-          console.log(data.forecast);
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+          // console.log(data.location);
+          // console.log(data.forecast);
         }
       });
     }
