2b1d5676712ed72baf8b4935186849f0b22f9303
jimichhatrala <jimichhatrala8488@gmail.com>
2022-07-09 17:01:57 +0530

    Reading documents from mongodb collection

--- node-course/task-manager/mongodb.js
+++ node-course/task-manager/mongodb.js
@@ -9,13 +9,13 @@ const { MongoClient, ObjectID } = require("mongodb");
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
-const id = new ObjectID();
-console.log(id);
-console.log(id.id);
-console.log(id.id.length);
-console.log(id.toHexString());
-console.log(id.toHexString().length);
-console.log(id.getTimestamp());
+// const id = new ObjectID();
+// console.log(id);
+// console.log(id.id);
+// console.log(id.id.length);
+// console.log(id.toHexString());
+// console.log(id.toHexString().length);
+// console.log(id.getTimestamp());
 
 MongoClient.connect(
   connectionURL,
@@ -26,6 +26,57 @@ MongoClient.connect(
     }
     console.log("Connected correctly");
     const db = client.db(databaseName);
+
+    db.collection("tasks").findOne(
+      { _id: new ObjectID("62c5be86f61fc1045e66cd58") },
+      (error, task) => {
+        if (error) {
+          return console.log("Unable to fetch");
+        }
+
+        console.log(task);
+      }
+    );
+
+    db.collection("tasks")
+      .find({ completed: false })
+      .toArray((error, tasks) => {
+        if (error) {
+          return console.log("Unable to fetch");
+        }
+        console.log(tasks);
+      });
+
+    // db.collection("users")
+    //   .find({ age: 21 })
+    //   .toArray((error, users) => {
+    //     if (error) {
+    //       return console.log("Unable to fetch");
+    //     }
+    //     console.log(users);
+    //   });
+
+    // db.collection("users")
+    //   .find({ age: 21 })
+    //   .count((error, count) => {
+    //     if (error) {
+    //       return console.log("Unable to fetch");
+    //     }
+    //     console.log(count);
+    //   });
+
+    // db.collection("users").findOne({ name: "Jimi" }, (error, user) => {
+    // db.collection("users").findOne({ name: "Jimi", age: 1 }, (error, user) => {
+    // db.collection("users").findOne(
+    //   { _id: new ObjectID("62c95dc00b12ce2b5e54d582") },
+    //   (error, user) => {
+    //     if (error) {
+    //       return console.log("Unable to fetch");
+    //     }
+
+    //     console.log(user);
+    //   }
+    // );
 
     // db.collection("users").insertOne(
     //   {
