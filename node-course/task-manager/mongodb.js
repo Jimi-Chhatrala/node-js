e88d9e5a9441f25190ae0908de8abb962cd31078
jimichhatrala <jimichhatrala8488@gmail.com>
2022-07-09 16:31:41 +0530

    Create record into db and explore GUId of mongodb

--- node-course/task-manager/mongodb.js
+++ node-course/task-manager/mongodb.js
@@ -1,10 +1,21 @@
 // CRUD create read update delete
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
+
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.id);
+console.log(id.id.length);
+console.log(id.toHexString());
+console.log(id.toHexString().length);
+console.log(id.getTimestamp());
 
 MongoClient.connect(
   connectionURL,
@@ -13,12 +24,80 @@ MongoClient.connect(
     if (error) {
       return console.log("Unable to connect to database!");
     }
-    // console.log("Connected correctly");
+    console.log("Connected correctly");
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-      name: "Jimi",
-      age: 20,
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     _id: id,
+    //     name: "Vikram",
+    //     age: 29,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user");
+    //     }
+
+    //     console.log("Result ops: ", result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Jimmy",
+    //     age: 21,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user");
+    //     }
+
+    //     console.log("Result ops: ", result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Ram",
+    //       age: 24,
+    //     },
+    //     {
+    //       name: "Shyam",
+    //       age: 26,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user");
+    //     }
+
+    //     console.log(result.insertedIds);
+    //   }
+    // );
+
+    // db.collection("tasks").insertMany(
+    //   [
+    //     {
+    //       description: "Clean the house",
+    //       completed: true,
+    //     },
+    //     {
+    //       description: "Renew inspection",
+    //       completed: false,
+    //     },
+    //     {
+    //       description: "Pot plants",
+    //       completed: false,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert taksk!");
+    //     }
+
+    //     console.log(result.insertedIds);
+    //   }
+    // );
   }
 );
